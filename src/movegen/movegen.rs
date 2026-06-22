2849fced4289e8585da552de17fc0fd02bcf75f7
AnanyTanwar <ananytanwar@gmail.com>
2026-06-22 12:32:00 +0530

    Add bishop, rook, and queen pseudo-legal move generation via magic tables

--- src/movegen/movegen.rs
+++ src/movegen/movegen.rs
@@ -1,5 +1,6 @@
-use crate::board::bitboard::{Bitboard, pop_lsb};
-use crate::board::position::{Color, PieceType, Position};
+use crate::board::bitboard::pop_lsb;
+use crate::board::position::{PieceType, Position};
+use crate::movegen::magic::MAGIC_TABLES;
 use crate::movegen::moves::{Move, MoveList};
 use crate::movegen::tables::KING_ATTACKS;
 use crate::movegen::tables::KNIGHT_ATTACKS;
@@ -38,6 +39,59 @@ pub fn generate_king_moves(pos: &Position, list: &mut MoveList) {
     }
 }
 
+pub fn generate_bishop_moves(pos: &Position, list: &mut MoveList) {
+    let us = pos.side_to_move;
+    let mut bishops = pos.pieces[us as usize][PieceType::Bishop as usize];
+    let own_pieces = pos.occupied_by[us as usize];
+    let occupied = pos.occupied;
+
+    while bishops != 0 {
+        let from = pop_lsb(&mut bishops);
+        let mut targets = MAGIC_TABLES.bishop_attacks(from as usize, occupied) & !own_pieces;
+
+        while targets != 0 {
+            let to = pop_lsb(&mut targets);
+            list.push(Move::new(from, to));
+        }
+    }
+}
+
+pub fn generate_rook_moves(pos: &Position, list: &mut MoveList) {
+    let us = pos.side_to_move;
+    let mut rooks = pos.pieces[us as usize][PieceType::Rook as usize];
+    let own_pieces = pos.occupied_by[us as usize];
+    let occupied = pos.occupied;
+
+    while rooks != 0 {
+        let from = pop_lsb(&mut rooks);
+        let mut targets = MAGIC_TABLES.rook_attacks(from as usize, occupied) & !own_pieces;
+
+        while targets != 0 {
+            let to = pop_lsb(&mut targets);
+            list.push(Move::new(from, to));
+        }
+    }
+}
+
+pub fn generate_queen_moves(pos: &Position, list: &mut MoveList) {
+    let us = pos.side_to_move;
+    let mut queens = pos.pieces[us as usize][PieceType::Queen as usize];
+    let own_pieces = pos.occupied_by[us as usize];
+    let occupied = pos.occupied;
+
+    while queens != 0 {
+        let from = pop_lsb(&mut queens);
+        let bishop_part = MAGIC_TABLES.bishop_attacks(from as usize, occupied);
+        let rook_part = MAGIC_TABLES.rook_attacks(from as usize, occupied);
+        let mut targets = (bishop_part | rook_part) & !own_pieces;
+
+        while targets != 0 {
+            let to = pop_lsb(&mut targets);
+            list.push(Move::new(from, to));
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -72,5 +126,29 @@ mod tests {
         let mut list = MoveList::new();
         generate_king_moves(&pos, &mut list);
         assert_eq!(list.len(), 8);
+    }
+
+    #[test]
+    fn test_bishop_moves_center_open_board() {
+        let pos = Position::from_fen("8/8/8/4B3/8/8/8/8 w - - 0 1").unwrap();
+        let mut list = MoveList::new();
+        generate_bishop_moves(&pos, &mut list);
+        assert_eq!(list.len(), 13);
+    }
+
+    #[test]
+    fn test_rook_moves_corner_open_board() {
+        let pos = Position::from_fen("R7/8/8/8/8/8/8/8 w - - 0 1").unwrap();
+        let mut list = MoveList::new();
+        generate_rook_moves(&pos, &mut list);
+        assert_eq!(list.len(), 14);
+    }
+
+    #[test]
+    fn test_queen_moves_center_open_board() {
+        let pos = Position::from_fen("8/8/8/4Q3/8/8/8/8 w - - 0 1").unwrap();
+        let mut list = MoveList::new();
+        generate_queen_moves(&pos, &mut list);
+        assert_eq!(list.len(), 27);
     }
 }
